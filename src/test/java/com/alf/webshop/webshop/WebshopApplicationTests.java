d13ac9e7f31b7237b3510c515fa018b59d836734
Tomi <pintertamas99@gmail.com>
2022-03-12 13:24:30 +0100

    init

---
+++ src/test/java/com/alf/webshop/webshop/WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alf.webshop.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
