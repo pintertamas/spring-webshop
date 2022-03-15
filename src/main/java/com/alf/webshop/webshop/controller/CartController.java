eaa9c29538327d9c570735c5b580b368f0e0ecab
Pinter Tamas <pintertamas99@gmail.com>
2022-03-15 14:35:12 +0100

    cart service update

--- src/main/java/com/alf/webshop/webshop/controller/CartController.java
+++ src/main/java/com/alf/webshop/webshop/controller/CartController.java
@@ -1,12 +1,8 @@
 package com.alf.webshop.webshop.controller;
 
-import com.alf.webshop.webshop.config.JwtTokenUtil;
-import com.alf.webshop.webshop.entity.Cart;
 import com.alf.webshop.webshop.entity.Item;
-import com.alf.webshop.webshop.entity.User;
 import com.alf.webshop.webshop.model.IdRequest;
-import com.alf.webshop.webshop.repository.CartRepository;
-import com.alf.webshop.webshop.repository.ItemRepository;
+import com.alf.webshop.webshop.service.CartService;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,30 +19,13 @@ import javax.validation.Valid;
 public class CartController {
 
     @Autowired
-    private CartRepository cartRepository;
-
-    @Autowired
-    private ItemRepository itemRepository;
-
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
+    private CartService cartService;
 
     @PostMapping("/add")
     public ResponseEntity<?> addItemToCart(@Valid @RequestBody IdRequest request) {
-        String token = JwtTokenUtil.getToken();
-        User user = jwtTokenUtil.getUserFromToken(token);
-        Cart cart = cartRepository.findCartById(user.getCart().getId());
-        Item item = itemRepository.findItemById(request.getId());
-
-        if (cart == null || item == null) {
-            LogFactory.getLog(this.getClass()).error("CART OR ITEM COULD NOT BE FOUND!");
-            return new ResponseEntity<>("Cart or item could not be found!", HttpStatus.NOT_FOUND);
-        }
-
         try {
-            cart.addItem(item);
-            cartRepository.save(cart);
-            return new ResponseEntity<>(HttpStatus.OK);
+            Item item = cartService.addItemToCart(request);
+            return new ResponseEntity<>(item, HttpStatus.OK);
         } catch (Exception e) {
             LogFactory.getLog(this.getClass()).error(e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
---
+++ src/main/java/com/alf/webshop/webshop/service/CartService.java
@@ -0,0 +1,52 @@
+package com.alf.webshop.webshop.service;
+
+import com.alf.webshop.webshop.config.JwtTokenUtil;
+import com.alf.webshop.webshop.entity.Cart;
+import com.alf.webshop.webshop.entity.Item;
+import com.alf.webshop.webshop.entity.User;
+import com.alf.webshop.webshop.exception.CartNotFoundException;
+import com.alf.webshop.webshop.exception.ItemNotFoundException;
+import com.alf.webshop.webshop.model.IdRequest;
+import com.alf.webshop.webshop.repository.CartRepository;
+import com.alf.webshop.webshop.repository.ItemRepository;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    public Item addItemToCart(IdRequest request) throws Exception {
+        String token = JwtTokenUtil.getToken();
+        User user = jwtTokenUtil.getUserFromToken(token);
+        Cart cart = cartRepository.findCartById(user.getCart().getId());
+        Item item = itemRepository.findItemById(request.getId());
+
+        if (cart == null) {
+            throw new CartNotFoundException(user.getCart());
+        } else if (item == null) {
+            throw new ItemNotFoundException(request.getId());
+        }
+
+        try {
+            cart.addItem(item);
+            cartRepository.save(cart);
+            return item;
+        } catch (Exception e) {
+            throw new Exception();
+        }
+    }
+
+
+}
