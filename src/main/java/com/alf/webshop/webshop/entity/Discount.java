9f669bc710dd372c6b201cb4e05fc23abd927939
Tomi <pintertamas99@gmail.com>
2022-03-22 22:26:53 +0100

    spring validation

--- src/main/java/com/alf/webshop/webshop/entity/Discount.java
+++ src/main/java/com/alf/webshop/webshop/entity/Discount.java
@@ -5,6 +5,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.sql.Date;
 
 @Getter
@@ -17,15 +21,25 @@ public class Discount {
     @GeneratedValue
     private Long id;
 
+    @NotNull(message = "Discount name cannot be null")
+    @NotBlank(message = "Discount name cannot be blank")
     @Column(name = "name")
     private String name;
 
+    @NotNull(message = "Discount description cannot be null")
+    @NotBlank(message = "Discount description cannot be blank")
     @Column(name = "description")
     private String description;
 
+    @NotNull(message = "Discount percent cannot be null")
+    @NotBlank(message = "Discount percent cannot be blank")
+    @Min(1)
+    @Max(100)
     @Column(name = "discount_percent")
     private int discountPercent;
 
+    @NotNull(message = "Discount end date cannot be null")
+    @NotBlank(message = "Discount end date cannot be blank")
     @Column(name = "end_date")
     private Date endDate;
 }
--- src/main/java/com/alf/webshop/webshop/entity/Item.java
+++ src/main/java/com/alf/webshop/webshop/entity/Item.java
@@ -4,8 +4,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,27 +25,39 @@ public class Item {
     @GeneratedValue
     private Long id;
 
+    @NotNull(message = "Item name cannot be null")
+    @NotBlank(message = "Item name cannot be blank")
     @Column(name = "name", nullable = false)
     private String name;
 
+    @NotNull(message = "Item description cannot be null")
+    @Length(max = 100, message = "Username must be between 4-20 characters")
     @Column(name = "description")
     private String description;
 
+    @NotNull(message = "Item price cannot be null")
+    @Min(0)
     @Column(name = "price", nullable = false)
     private double price;
 
+    @NotNull(message = "Item color cannot be null")
     @Column(name = "color", nullable = false)
     private Color color;
 
+    @NotNull(message = "Item gender cannot be null")
     @Column(name = "gender", nullable = false)
     private Gender gender;
 
+    @NotNull(message = "Item size cannot be null")
     @Column(name = "size", nullable = false)
     private Size size;
 
+    @NotNull(message = "Item SKU cannot be null")
+    @Min(1)
     @Column(name = "SKU", nullable = false)
     private int sku;
 
+    @NotNull(message = "Item category cannot be null")
     @Column(name = "category")
     private Category category;
 
